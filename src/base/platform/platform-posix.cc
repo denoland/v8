f6ebf8c3110283cec8f67c9008234bda01967d8b
Bert Belder <bertbelder@gmail.com>
2022-05-25 20:40:04 +0200

    Fix crash on Apple Silicon when mprotect() fails expectedly

--- src/base/platform/platform-posix.cc
+++ src/base/platform/platform-posix.cc
@@ -479,11 +479,6 @@ bool OS::SetPermissions(void* address, size_t size, MemoryPermission access) {
   int prot = GetProtectionFromMemoryPermission(access);
   int ret = mprotect(address, size, prot);
 
-  // Any failure that's not OOM likely indicates a bug in the caller (e.g.
-  // using an invalid mapping) so attempt to catch that here to facilitate
-  // debugging of these failures.
-  if (ret != 0) CHECK_EQ(ENOMEM, errno);
-
   // MacOS 11.2 on Apple Silicon refuses to switch permissions from
   // rwx to none. Just use madvise instead.
 #if defined(V8_OS_DARWIN)
@@ -492,6 +487,11 @@ bool OS::SetPermissions(void* address, size_t size, MemoryPermission access) {
     return ret == 0;
   }
 #endif
+
+  // Any failure that's not OOM likely indicates a bug in the caller (e.g.
+  // using an invalid mapping) so attempt to catch that here to facilitate
+  // debugging of these failures.
+  if (ret != 0) CHECK_EQ(ENOMEM, errno);
 
   if (ret == 0 && access == OS::MemoryPermission::kNoAccess) {
     // This is advisory; ignore errors and continue execution.
